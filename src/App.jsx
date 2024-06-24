da829c7e13192bbc40dda8a9e36acb0b6ea0b592
[Aman Yadav] <[aman4864y@gmail.com]>
2024-06-24 20:04:09 +0530

    modified:   src/App.css 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import React from "react";
-import "./App.css";
-import pic1 from "../public/image/pic1.png"; // Adjust the path according to your file structure
+// import React from 'react';
+// import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+import "./App.css"
 
 function App() {
-  const h1Styles = {
-    backgroundImage: `url(${pic1})`,
-    backgroundSize: "40%",
-    backgroundPosition: "50% 50%",
-    WebkitBackgroundClip: "text",
-    color: "hsla(0, 0%, 0%, 0.08)",
-    animation: "zoomout 10s ease 500ms forwards",
-    fontSize: "10vw",
-  };
-
   return (
-    <>
-      <h1 style={h1Styles}>Anvisha</h1>
-    </>
-  );
+  <>
+  <h1>Anvisha</h1>
+  </>
+    );
 }
 
 export default App;
