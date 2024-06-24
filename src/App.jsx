6bb756b38c8460337ea7737b7da5e919dcc33c57
[Aman Yadav] <[aman4864y@gmail.com]>
2024-06-24 19:45:08 +0530

    maanjaayaar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+import "./App.css"
+
+function App() {
+  return (
+  <>
+  {/* <h1>Happy</h1>
+  <h1>Birthday</h1> */}
+  <h1>Anvisha</h1>
+  </>
+    );
+}
+
+export default App;
